0f9535ecd19a3cfe01a141cfc1f2838543f197de
Anurag1707 <Anuragjhariya1707@gmail.com>
2025-09-23 21:18:31 +0530

    C

--- Looping/primeno.c
+++ Looping/primeno.c
@@ -1,34 +1,26 @@
-// #include<stdio.h>
-// int main()
-//  {
-//     int p=1;
-//     int i=2;
-//     int n=5;
-
-//    // if(n<=1){
-//      // printf("nahii");
-//     //}
-
+#include<stdio.h>
+int main()
+ {
+    int p=1;
+    int i=2;
+    int n=5;
+    if(n<=1){
+      printf("nahii");
+    }
 
     
-    
-//     if(n<=1){
-//       printf("nahii");
-//     }
+    while(i<=n){
+      if(n%i==0){
+        p=0;
+        break;
+      }
+    }
 
-    
-//     while(i<=n){
-//       if(n%i==0){
-//         p=0;
-//         break;
-//       }
-//     }
+    if(p)
+    {
+      printf("nhi h");
+    }else{
+      printf("hai");
+    }
 
-//     if(p)
-//     {
-//       printf("nhi h");
-//     }else{
-//       printf("hai");
-//     }
-// // return 0;
-//   }     
+  }     
---
+++ pointer/swapwithoutv.c
@@ -0,0 +1,12 @@
+// #include<stdio.h>
+// int swap(int *x,int *y){
+//     *x=*x+*y;
+//     *y=*x-*y;
+//     *x=*x-*y;
+//     printf("%d\t%d",*x,*y);
+// }
+// int main(){
+//     int x=10;
+//     int y=30;
+//     swap(&x,&y);
+// }
