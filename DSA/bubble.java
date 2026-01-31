86b1cf995fe2eae0abd195978dbc8a4a0349af47
Anurag1707 <Anuragjhariya1707@gmail.com>
2026-01-31 12:39:33 +0530

    bubble

---
+++ DSA/bubble.java
@@ -0,0 +1,23 @@
+
+class Bubble {
+    public static void main(String[] args) {
+        int[] arr = {5, 3, 8, 4, 2};
+        int n = arr.length;
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    // swap
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        // print sorted array
+        for (int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
