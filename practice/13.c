ef4dc173f4d75773eed753008b7dff98dd437c73
Anurag1707 <Anuragjhariya1707@gmail.com>
2026-02-05 13:30:15 +0530

    13

---
+++ practice/13.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int a = 100;
+    int *p = &a;
+    int **pp = &p;
+
+    printf("Value of a = %d\n", a);
+    printf("Using *p = %d\n", *p);
+    printf("Using **pp = %d\n", **pp);
+
+    return 0;
+}
